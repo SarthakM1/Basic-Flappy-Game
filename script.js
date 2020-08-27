b33ad285e08c80123330e80e0e89672e078b0431
SarthakM01 <sarthak.mahajan54@yahoo.com>
2020-08-27 17:11:44 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
-    var random = -((Math.random()*300)+150);
-    hole.style.top = random + "px";
+    var random = Math.random()*3;
+    var top = (random*100)+150;
+    hole.style.top = -(top) + "px";
 });
+setInterval(function(){
+    var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top = (characterTop+3)+"px";
+},10);
+
+function jump(){
+    jumping = 1;
+
+    var jumpInterval = setInterval(function(){
+
+    },10);
+}
